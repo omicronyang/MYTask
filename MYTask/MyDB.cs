95ae19424d69e2c80e2d20c0bb4c67b216c8c330
omicron <tigeryyf@qq.com>
2015-11-28 15:35:35 +0800

    Revert "第一次提交"
    This reverts commit 77e792d815b157ee1950373df54f4b7960f7b21d.

---
+++ MYTask/MyDB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace MYTask
+{
+    class MyDB
+    {
+
+        private MySqlConnection OnlineDBase;
+        private SQLiteConnection LocalDBase;
+        private string ConnectCommand;
+        public int Online; //0 for offline, 1 for online
+
+        public MyDB()
+        {
+            string server = "qdm183517592.my3w.com";
+            string uid = "qdm183517592";
+            string upw = "sssuittest";
+            string dbname = "qdm183517592_db";
+            ConnectCommand = String.Format("server = {0}; user id = {1}; password ={2}; database = {3}; pooling = false",
+                server, uid, upw, dbname);
+        }
+
+        public bool Connect()
+        {
+            if (OnlineDBase != null) OnlineDBase.Close();
+            try
+            {
+                OnlineDBase = new MySqlConnection(ConnectCommand);
+                OnlineDBase.Open();
+                Online = 1;
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //MessageBox.Show(ex.ErrorCode + "Error connecting to the server: " + ex.Message);
+                Online = 0;
+                return false;
+            }
+        }
+
+        public void Close()
+        {
+            if (OnlineDBase != null) OnlineDBase.Close();
+        }
+    }
+}
