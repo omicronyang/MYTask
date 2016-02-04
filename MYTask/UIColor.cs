0d08d164eb6032485a6f9f11832281d8bfdffd57
omicron <tigeryyf@qq.com>
2016-02-04 14:02:19 +0800

    优化颜色控制模块

--- MYTask/UIColor.cs
+++ MYTask/UIColor.cs
@@ -9,15 +9,15 @@ namespace MYTask
     public class UIColor
     {
         public Color MainColor;
-        public Color MouseOverColor;
-        public Color MouseDownColor;
+        public Color MOC;
+        public Color MDC;
         public int ColorType;
 
         public UIColor()
         {
             MainColor = Color.RoyalBlue;
-            MouseOverColor = Color.CornflowerBlue;
-            MouseDownColor = Color.MidnightBlue;
+            MOC = Color.CornflowerBlue;
+            MDC = Color.MidnightBlue;
             ColorType = 1;
         }
 
@@ -30,49 +30,24 @@ namespace MYTask
             ColorType = NewColorType;
             switch (ColorType)
             {
-                case 1:
-                    {
-                        MainColor = Color.RoyalBlue;
-                        MouseOverColor = Color.CornflowerBlue;
-                        MouseDownColor = Color.MidnightBlue;
-                        break;
-                    }
-                case 2:
-                    {
-                        MainColor = Color.SeaGreen;
-                        MouseOverColor = Color.MediumSeaGreen;
-                        MouseDownColor = Color.DarkGreen;
-                        break;
-                    }
-                case 3:
-                    {
-                        MainColor = Color.DarkMagenta;
-                        MouseOverColor = Color.MediumOrchid;
-                        MouseDownColor = Color.Purple;
-                        break;
-                    }
-                case 4:
-                    {
-                        MainColor = Color.Crimson;
-                        MouseOverColor = Color.LightCoral;
-                        MouseDownColor = Color.Brown;
-                        break;
-                    }
-                case 5:
-                    {
-                        MainColor = Color.OrangeRed;
-                        MouseOverColor = Color.Coral;
-                        MouseDownColor = Color.Brown;
-                        break;
-                    }
-                case 6:
-                    {
-                        MainColor = Color.DimGray;
-                        MouseOverColor = Color.Gray;
-                        MouseDownColor = Color.Black;
-                        break;
-                    }
+                case 1: { MainColor = Color.RoyalBlue; break; }
+                case 2: { MainColor = Color.SeaGreen; break; }
+                case 3: { MainColor = Color.DarkMagenta; break; }
+                case 4: { MainColor = Color.Crimson; break; }
+                case 5: { MainColor = Color.OrangeRed; break; }
+                case 6: { MainColor = Color.DimGray; break; }
             }
+            int R = MainColor.R;
+            int G = MainColor.G;
+            int B = MainColor.B;
+            int oR = (R <= 204) ? R + 51 : R;
+            int oG = (G <= 204) ? G + 51 : G;
+            int oB = (B <= 204) ? B + 51 : B;
+            int dR = (R >= 51) ? R - 51 : R;
+            int dG = (G >= 51) ? G - 51 : G;
+            int dB = (B >= 51) ? B - 51 : B;
+            MOC = Color.FromArgb(oR, oG, oB);
+            MDC = Color.FromArgb(dR, dG, dB);
         }
 
         public void UpdateColor(Color NewMainColor)
@@ -82,7 +57,47 @@ namespace MYTask
             else if (NewMainColor == Color.DarkMagenta) UpdateColor(3);
             else if (NewMainColor == Color.Crimson) UpdateColor(4);
             else if (NewMainColor == Color.OrangeRed) UpdateColor(5);
+            else if (NewMainColor == Color.DimGray) UpdateColor(6);
         }
 
     }
+
+    public class CalendarClr
+    {
+        public Color MainColor;
+        public Color MOC;
+        public Color MDC;
+        public int ColorType;
+
+        public void UpdateColor(int NewClrType)
+        {
+            ColorType = NewClrType;
+            switch (ColorType)
+            {
+                case 0: { MainColor = Color.DimGray; break; }
+                case 4: { MainColor = Color.FromArgb(153, 102, 153); break; } //策划中
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9: { MainColor = Color.FromArgb(153, 255, 0); break; }   //进行
+                case 14: { MainColor = Color.FromArgb(0, 153, 0); break; }    //完成
+                case 22: { MainColor = Color.Red; break; }                    //中断
+                case 23: { MainColor = Color.FromArgb(255, 204, 0); break; }  //推迟
+            }
+            int R = MainColor.R;
+            int G = MainColor.G;
+            int B = MainColor.B;
+            int oR = (R <= 204) ? R + 51 : R;
+            int oG = (G <= 204) ? G + 51 : G;
+            int oB = (B <= 204) ? B + 51 : B;
+            int dR = (R >= 51) ? R - 51 : R;
+            int dG = (G >= 51) ? G - 51 : G;
+            int dB = (B >= 51) ? B - 51 : B;
+            MOC = Color.FromArgb(oR, oG, oB);
+            MDC = Color.FromArgb(dR, dG, dB);
+        }
+
+    }
+
 }
